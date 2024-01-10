33fc20c9b546d606686df9f233eaf480ad5a4657
Weverton Davi <wevertondavi.farias@gmail.com>
2024-01-10 16:25:13 -0300

    Desafio de POO concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphon Nokia: ");
+Smartphone nokia = new Nokia(modelo: "modelo c53", numero: "88210974", imie: "11111",memoria: 128 );
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphon Iphone:");
+Smartphone iphone = new Iphone(modelo: "iphone 15", numero: "85158967", imie: "15639",memoria: 128 );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
